76bd8639c8c23dd6fd07e2f9f15ae0af75e3b15b
Ismatulla <ibragimovismatulla@gmail.com>
2023-01-07 01:41:08 +0500

    Update Project

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿
-string greeting="Salom, yangi foydalanuvchi, ahvollaringiz qalay!";
+﻿string greeting="Salom, yangi foydalanuvchi, ahvollaringiz qalay!";
 Console.WriteLine(greeting);
 Console.Write("Iltimos, ismingizni kiriting: ");
 string userName = Console.ReadLine();
@@ -10,6 +9,6 @@ System.Console.WriteLine($"Kesha to'tiqushining yoshi {keschaAge}da");
 System.Console.Write("Sizning yoshingiz nechchida?: ");
 string ageAastring = Console.ReadLine();
 int age = Convert.ToInt32(ageAastring);
-System.Console.WriteLine($"Barakalla {userName} siz {2022-age}-yilda tug'ilgansiz!");
+System.Console.WriteLine($"Barakalla {userName} siz {2023-age}-yilda tug'ilgansiz!");
 System.Console.WriteLine($"Kesha to'tiqushi va  sizning yoshingiz o'rtasidagi farq {age-keschaAge} yoshni tashkil qiladi");
 
